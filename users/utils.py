b184e762719245cf3a63308fe0a412f62916273c
JungWooYoung <wyjung0731@gmail.com>
2022-09-18 11:49:32 +0900

    Change serviceAccountKey.json

--- users/utils.py
+++ users/utils.py
@@ -97,11 +97,11 @@ def send_to_firebase_cloud_messaging():
 
     # See documentation on defining a message payload.
     message = messaging.Message(
-    notification=messaging.Notification(
-        title='안녕하세요 타이틀 입니다',
-        body='안녕하세요 메세지 입니다',
-    ),
-    token=registration_token,
+        notification=messaging.Notification(
+            title='안녕하세요 타이틀 입니다',
+            body='안녕하세요 메세지 입니다',
+        ),
+        token=registration_token,
     )
 
     response = messaging.send(message)
